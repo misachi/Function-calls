396cd1d7846d7c92dece797349121025046445ff
Brian Misachi <misachibrian@gmail.com>
2020-08-13 09:41:12 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+
+int foo(int n) {
+  return n * 2;
+}
+
+int main() {
+  std::clock_t start, end;
+  int size = 100000;
+  double j;
+  std::cout << "Start loop with function calls"<< std::endl;
+  start = std::clock();
+  for (size_t i=0; i < size; ++i)
+    j = foo(i);
+
+  end = std::clock();
+
+  std::cout << "Fcalls running time -> " <<1000.0 * (end - start)/CLOCKS_PER_SEC << std::endl;
+
+
+  std::cout << std::endl;
+
+  std::cout << "Start loop no function calls"<< std::endl;
+
+  start = std::clock();
+  for (size_t i=0; i < size; ++i) {
+    j = i * 3;
+  }
+  end = std::clock();
+
+  std::cout << "No fcalls running time -> " <<1000.0 * (end - start)/CLOCKS_PER_SEC << std::endl;
+}
